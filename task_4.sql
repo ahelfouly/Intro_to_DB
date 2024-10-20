11cacd404dc2483e1567c75baa5095dccf7e9fe5
Ahmed Elfouly <ahmedelfouly1999@gmail.com>
2024-10-20 19:31:38 +0300

    Add task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- task_4.sql
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE,
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
